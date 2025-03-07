f1990bfd2a51d384621184ac2855fe4c0da5d8d5
aneri <aneriavala26@gmail.com>
2025-03-07 18:38:59 +0530

    Step: 3 with cli-inputs

--- index.ts
+++ index.ts
@@ -1,6 +1,31 @@
+import * as readline from "readline";
+
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 const add = (numbers: string = ""): number => {
     if (!numbers) return 0;
-    return numbers.split(",").map(Number).reduce((sum, num) => sum + num, 0);
+    return numbers.split(/,|\n/).map(Number).reduce((sum, num) => sum + num, 0);
 };
 
-console.log(add("1,5"));
+let userInput = "";
+
+rl.on("line", (line) => {
+    if (line === "") {  
+        console.log(`\nInput: \n"${userInput}"`);
+        
+        try {
+            const result = add(userInput);
+            console.log(`Output: ${result}`);
+        } catch (error) {
+            console.error(`Error: ${error.message}`);
+        }
+
+        rl.close();
+    } else {
+        userInput += (userInput ? "\n" : "") + line; 
+    }
+});
